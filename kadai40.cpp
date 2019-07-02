0d94d85e37f20c30cacbc16006dd0b03fa1b63b4
18D8104032K <18D8104032K@ISE44c.educ.ise.chuo-u.ac.jp>
2019-07-02 17:33:35 +0900

    kadai

--- kadai40.cpp
+++ kadai40.cpp
@@ -6,20 +6,26 @@
 
 int main(){
   std::string directory_names = std::getenv("PATH");
-  int start = 0,end = 0;
+  int start = 0,end = 1;
   int size = directory_names.size();
-  
+  int j = 1;
   for(int i = 0;i < size;i++){
     if(directory_names[i] == ':'){
-      std::cout << directory_names.substr(start,end) << "\n";
+      if(j == 1){
+      std::cout << j << ")" << directory_names.substr(start,end-1) << "\n";
+      }else{
+	std::cout << j << ")" << directory_names.substr(start,end) << "\n";
+      }
       start = i + 1;
-      end = i + 1;
-      i = i + 1;
+      end = 0;
+      //i = i + 1;
+      j++;
       //std::cout << start << " "  << end << "\n";
     }else{
       end++;
       //std::cout << start << " " << end << "\n";
     }
   }
+  std::cout << j << ")" << directory_names.substr(start,size-start) << "\n";
   return 0;
 }
